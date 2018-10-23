a6b5f6e354726528c0da116988ab47d9ec3e9abf
ontanj <anton.jeppsson@outlook.com>
2018-10-24 00:28:50 +0200

    sort items in results

--- results.py
+++ results.py
@@ -9,12 +9,18 @@ class SysResults:
         if not isinstance(item, SysItem):
             raise TypeError("Not a SysItem.")
         self.items.append(item)
+        return self
     
     def __iter__(self):
         return iter(self.items)
 
-    def sort(self, key="apk"):
-        self.item.sort(key=lambda x: x[key])
+    def sort(self, key="apk", reverse=False):
+        if key == "apk":
+            reverse = not reverse
+        self.items.sort(key=lambda x: x[key], reverse=reverse)
+
+    def __getitem__(self,index):
+        return self.items[index]
 
 
     
--- test_results.py
+++ test_results.py
@@ -26,7 +26,21 @@ class TestResults(unittest.TestCase):
                 item2 = r[index+1]
             except IndexError:
                 break
-            self.assertGreater(item.data["apk"], item2)
+            self.assertGreater(item.data["apk"], item2.data["apk"])
+
+    def test_sort_by_price(self):
+        r = SysResults()
+        i1 = SysItem(**{"apk": 6, "price": 8})
+        i2 = SysItem(**{"apk": 8, "price": 5})
+        i3 = SysItem(**{"apk": 4, "price": 10})
+        r.add_item(i1).add_item(i2).add_item(i3)
+        r.sort(key="price")
+        for index,item in enumerate(r):
+            try:
+                item2 = r[index+1]
+            except IndexError:
+                break
+            self.assertLess(item.data["price"], item2.data["price"])
 
 if __name__ == "__main__":
     unittest.main()
